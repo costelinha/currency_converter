f1621796dcfdb51a399b5ce00cfb0b3d4c53d515
Andre <andregomesngr@gmail.com>
2020-06-17 18:56:08 -0500

    Starting currency inversion

--- app/javascript/exchanges.js
+++ app/javascript/exchanges.js
@@ -4,3 +4,15 @@ document.addEventListener('turbolinks:load', function() {
     document.getElementById('result').value = result.value
   })
 })
+
+/*  Inversion logic(pedding)
+
+document.addEventListener('turbolinks:load', function () {
+  document.getElementById('exchange_form').addEventListener('ajax:success', function (event) {
+    a = document.getElementById('source_currency').value
+    document.getElementById('source_currency').value = document.getElementById('target_currency').value
+    document.getElementById('target_currency').value = a
+  })
+})
+
+*/
